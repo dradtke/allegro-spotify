6f3811bb0b23bf23f5a45641316f84ce9f9f7342
Damien Radtke <damienradtke@gmail.com>
2015-02-06 11:43:41 -0600

    Remove unnecessary header.

--- example/src/main.c
+++ example/src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <libspotify/api.h>
 
+#include <libspotify/api.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_spotify.h>
